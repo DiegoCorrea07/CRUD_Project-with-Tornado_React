2dfa495a8f1e31f5c6c73a26f4f5f81bf2fe8edc
DiegoCorrea07 <diego.correa.cumbicus07@outlook.com>
2025-04-16 00:53:45 -0500

    Actualización de las rutas con decoradores para el manejo de login y permisos.

--- backend/views/routes.py
+++ backend/views/routes.py
@@ -1,5 +1,7 @@
 import tornado.web
 from backend.controllers.flight_controller import FlightController
+from backend.controllers.auth_controller import AuthRegisterHandler, AuthLoginHandler
+from backend.utils.security import auth_required, admin_required
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -9,38 +11,44 @@ class FlightHandler(tornado.web.RequestHandler):
     controller = FlightController()
 
     def set_default_headers(self):
+        self.set_header("Content-Type", "application/json")
         self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with, Content-Type")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Authorization")
         self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
     def options(self, flight_id=None):
-        # Maneja las solicitudes preflight de CORS
         self.set_status(204)
         self.finish()
 
-    def get(self, flight_id=None):
+    @auth_required
+    async def get(self, flight_id=None):
         if flight_id:
             self.write(self.controller.get_by_id(flight_id))
         else:
             self.write(self.controller.get_all())
 
-    def post(self):
+    @auth_required
+    async def post(self):
         """Crear un nuevo vuelo."""
         flight_data = tornado.escape.json_decode(self.request.body)
         self.write(self.controller.create(flight_data))
 
-    def put(self, flight_id):
-        """Actualizar un vuelo existente."""
+    @admin_required
+    async def put(self, flight_id):
+        """Actualizar un vuelo existente (solo para admins)."""
         flight_data = tornado.escape.json_decode(self.request.body)
         self.write(self.controller.update(flight_id, flight_data))
 
-    def delete(self, flight_id):
-        """Eliminar un vuelo."""
+    @admin_required
+    async def delete(self, flight_id):
+        """Eliminar un vuelo (solo para admins)."""
         self.write(self.controller.delete(flight_id))
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/flights", FlightHandler),  # Para operaciones con todos los vuelos
-        (r"/flights/([0-9]+)", FlightHandler),  # Para operaciones con un vuelo específico
+        (r"/api/register", AuthRegisterHandler),
+        (r"/api/login", AuthLoginHandler),
+        (r"/api/flights", FlightHandler),  # Para operaciones con todos los vuelos
+        (r"/api/flights/([0-9]+)", FlightHandler),  # Para operaciones con un vuelo específico
     ])
